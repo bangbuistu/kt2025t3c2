8d85a33550e3894dd92e3f240b2ccd10e4f11ffc
unknown <tranvinh4464@gmail.com>
2025-11-18 10:26:11 +0700

    Hoan thanh viet KiemTraSoNguyen

---
+++ KiemTraSoNguyen.cpp
@@ -0,0 +1,13 @@
+ #inlcude <iostream>
+ using namespace std;
+ int main()
+ {
+	 int n;
+	 cout << "Nhap so nguyen: "
+	 cin >> n;
+	 if(n>0)
+		 cout << n << " La so duong "<< endl;
+	 else 
+		 cout << n << " La so am " << endl;
+	 return 0;
+ }
