390414960c76dd4dadf04d53554b3d5235cf713c
huudoi <doiphamcute@gmail.com>
2025-11-18 10:49:09 +0700

    kiemtrasonguyento

---
+++ ktsonguye.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int n;
+	cout<<"input number";
+	cin>>n;
+	if(n%2==0)
+		cout<<n<<"la so chan";
+	else 
+		co9ut<<n <<"la so le";
+	return 0;
+}
